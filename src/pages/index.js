236154ec1cee2ecf1293117019856da9963ed875
Emre Kelleci <>
2021-03-27 00:43:36 -0400

    First Commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
+import '../../stylesheets/style.scss'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return(
+    <div>
+
+      <p> Hello </p>
+
+    </div>
+  )
 }
