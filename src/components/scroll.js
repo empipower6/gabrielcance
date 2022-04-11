029c4d9baf9310c60a6efdce3ba84b00b9b7f21b
Emre Kelleci <35880247+empipower6@users.noreply.github.com>
2022-04-11 16:59:23 -0400

    height changes

--- src/components/scroll.js
+++ src/components/scroll.js
@@ -20,12 +20,9 @@ const Scroll = () => {
   }, [])
   const changeHeight = () => {
     // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-    let vh = window.innerHeight
+    let vh = window.innerHeight - 100
     setHeight(vh)
   }
-  useEffect(() => {
-    let vh = window.innerHeight * 0.01
-  })
 
   const checkWhere = () => {
     if (window.pageYOffset === 0) {
