02c6f906e06e4dbd5416b95eed651c2b4e9c43f4
Emre Kelleci <35880247+empipower6@users.noreply.github.com>
2022-04-13 14:47:50 -0400

    nvmrc done

--- src/components/scroll.js
+++ src/components/scroll.js
@@ -5,13 +5,39 @@ import { gsap } from "gsap/dist/gsap"
 
 const Scroll = () => {
   const [disappear, setDisappear] = useState(true)
+  const [height, setHeight] = useState(0)
   const scroll = useRef(null)
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
       checkWhere()
     })
+    // We listen to the resize event
+    window.addEventListener("resize", () => {
+      // We execute the same script as before
+      changeHeight()
+    })
+    changeHeight()
+    window.addEventListener("resize", changeHeight)
+    return () => window.removeEventListener("scroll", changeHeight)
   }, [])
+  const changeHeight = () => {
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight
+    setHeight(vh)
+  }
+  useEffect(() => console.log(height), [height])
+
+  const checkWhere = () => {
+    if (window.pageYOffset === 0) {
+      setDisappear(true)
+    } else {
+      if (disappear) {
+        setDisappear(false)
+      }
+    }
+  }
+
   useEffect(() => {
     const timeline = gsap.timeline({ paused: true })
     timeline.fromTo(
@@ -22,19 +48,27 @@ const Scroll = () => {
 
     disappear ? timeline.reverse(0.5) : timeline.play()
   }, [disappear])
-  const checkWhere = () => {
-    if (window.pageYOffset === 0) {
-      setDisappear(true)
-    } else {
-      if (disappear) {
-        setDisappear(false)
-      }
-    }
-  }
+
   return (
-    <div className="scrollDown" ref={scroll}>
-      <p>Scroll down</p>
-    </div>
+    <>
+      {/* <div
+        style={{
+          backgroundColor: "white",
+          height: "20px",
+          width: "100%",
+          marginTop: `${height - 20}px`,
+        }}
+      ></div> */}
+      <div
+        className="scrollDown"
+        ref={scroll}
+        style={{
+          paddingTop: `${Number(height) - 40}px`,
+        }}
+      >
+        <p>Scroll down</p>
+      </div>
+    </>
   )
 }
 
